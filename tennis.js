6bac6c5889fc014a6912096c1dee4a966f3be05a
EduardoAquinta <edwoodashton@yahoo.co.uk>
2023-10-30 16:10:18 +0000

    next tests passed

--- tennis.js
+++ tennis.js
@@ -1,5 +1,23 @@
-const tennisScore = () => {
-    return 1;
+class tennisScore {
+    constructor(player1Score, player2Score){
+        this.player1Score = player1Score;
+        this.player2Score = player2Score;
+    }
+  
+    point(player){
+        if(player === "player1Win"){ 
+            this.player1Score += 15 
+            console.log(player)
+        };
+        if(player === "player2Win"){
+            this.player2Score +=15
+            console.log(player)
+        }
+    } 
+
+    get score(){
+        return`${this.player1Score} - ${this.player2Score}`
+   }
 }
 
 module.exports = tennisScore;
--- tennis.test.js
+++ tennis.test.js
@@ -1,8 +1,18 @@
 const tennisScore = require("./tennis");
 
-describe("Tests to ensure tennisScore returns the correct score", () => {
-    it("Returns a number", () => {
-        let score = 1;
-        expect(typeof(tennisScore(score))).toBe("number")
+describe("The basic set up", () => {
+    it("There are two players, each with a score", () => {
+        const game = new tennisScore(0, 0)
+        expect(game.score).toBe("0 - 0")
     })
-})
+    it("adds 15 to player1,  who won the point", () => {
+        const game = new tennisScore(0, 0);
+        game.point("player1Win")
+        expect(game.score).toBe("15 - 0");
+    })
+    it("adds 15 to player2, who won the point", () => {
+        const game = new tennisScore(0, 0)
+        game.point("player2Win");
+        expect(game.score).toBe("0 - 15")
+    })
+});
